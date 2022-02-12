30a0b8757b109df7e486f8aa0430df37f7fdd09a
James <jameshuang337@gmail.com>
2022-02-12 14:58:08 -0800

    add primary keys to joining tables

--- server/db/schema/02_followers.sql
+++ server/db/schema/02_followers.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS followers CASCADE;
 
 CREATE TABLE followers (
+  PRIMARY KEY (user_id, follower_id),
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   follower_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
