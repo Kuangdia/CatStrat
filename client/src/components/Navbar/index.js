566f83c1482083d30a7249e5753dc69412c8fb15
AllenZZP <131452abc@gmail.com>
2022-02-13 15:51:52 -0800

    add notification number

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,16 +2,30 @@ import "./Navbar.scss";
 
 import FaceIcon from '@mui/icons-material/Face';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import PetsIcon from '@mui/icons-material/Pets';
+
 
 export default function Navbar(props) {
   return (
     <section className="navbar">
       <div className="navbar__button">
-        <NotificationsNoneIcon className="navbar__icon"/>
-        <PetsIcon className="navbar__icon"/>
+
+        <div className="navbar__notice">
+          <button>
+            <NotificationsNoneIcon className="navbar__icon" />
+          </button>
+          <p>+<span>666</span></p>
+        </div>
+
+        <div className="navbar__coin">
+          <img 
+            src="catecoin.png"
+            alt="catecoin-icon"
+            className="navbar__icon"
+          />
+          <p>X<span>100</span></p>
+        </div>
       </div>
-      
+
       <div className="navbar__user">
         <FaceIcon className="navbar__avatar navbar__icon" />
         <p className="navbar__user__name">Allen</p>
