5d850d788bbe6690151878cd7c3f520ef28b37d3
Diana <slorrrr@gmail.com>
2022-02-14 16:51:37 -0500

    dashboard logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import './App.scss';
 import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Landing from './pages/Landing';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />}/> */}
+        <Route path="/" element={<Landing />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/dashboard" element={<Dashboard />}/>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,48 @@
-import Sidebar from "../components/Sidebar"
+import React from 'react'
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Sidebar from "../components/Sidebar";
+import "../styles/dashboard.scss"
 
 export default function Dashboard(props) {
+  const userID = localStorage.getItem('userID')
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    
+    Axios.defaults.withCredentials = true;
+
+    if (userID) {
+      console.log("first")
+      Axios.post('http://localhost:8080/strategies', {userID})
+      .then(response => console.log(response))
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+
+  }, [])
+
+  // const getStrat = () => {
+  //   console.log("clicked")
+  //   Axios.defaults.withCredentials = true;
+
+  //   Axios.get('http://localhost:8080/strategies')
+  //   .then((response) => {
+  //     console.log("third")
+  //     console.log("get strats", response)
+  //     setData(response)
+  //   })
+  // }
+
   return(
-    <Sidebar />
+    <>
+      <div className="dash-container">
+        <Sidebar />
+        <div className="get">
+          {/* <button onClick={getStrat}>Button</button> */}
+        </div>
+      </div>
+    </>
   );
 }
