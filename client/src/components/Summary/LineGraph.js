36ad5360ed107f5787e9fdecc8019d305e4f63bc
James <jameshuang337@gmail.com>
2022-02-15 17:42:03 -0800

    create linegraph component

---
+++ client/src/components/Summary/LineGraph.js
@@ -0,0 +1,10 @@
+import { Line } from "react-chartjs-2";
+
+export default function LineGraph(props) {
+
+  return (
+    <section>
+      hello
+    </section>
+  );
+}
--- client/src/components/Summary/index.js
+++ client/src/components/Summary/index.js
@@ -1,9 +1,16 @@
 import './Summary.scss';
 import { useState } from 'react';
+import getTotalEarnings from '../../helpers/getTotalEarnings';
+import getAverageEarnings from '../../helpers/getAverageEarnings';
+
+import LineGraph from './LineGraph';
 
 export default function Summary(props) {
 
+  // Organize and manipulate data
   const data = props.data
+  const totalEarnings = getTotalEarnings(data);
+  const averageEarnings = getAverageEarnings(data);
 
   // Store data after being manipulated
   // const [displayData, setDisplayData] = useState({})
@@ -11,26 +18,26 @@ export default function Summary(props) {
   // Store user selection
   // const [selection, setSelection] = useState('All time')
   
-
+  
 
 
   return (
     <section>
-      
-      
       <div className="stats">
         <div className="stats__total-earnings">
-          Total Earnings
+          Total Earnings: ${totalEarnings}
         </div>
 
         <div className="stats__total-earnings">
-          Average Daily Earnings
+          Average Daily Earnings: ${averageEarnings}
         </div>
 
         <div className="stats__total-earnings">
           TBD
         </div>
       </div> 
+
+      <LineGraph data={data}/>
       
       
     </section>
