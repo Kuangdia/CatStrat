01b5db85510a6a59d5770613a41c773b97485a49
James <jameshuang337@gmail.com>
2022-02-17 00:40:25 -0800

    add helper to get pie chart data

---
+++ client/src/helpers/getPieChartData.js
@@ -0,0 +1,30 @@
+import { format, parseISO } from 'date-fns';
+// var parseISO = require('date-fns/parseISO');
+
+export default function getLineGraphData(data) {
+    const pieData = [];
+    const pieLabels = [];
+
+    let formattedData = data.reduce((acc, curr) => {
+        if (acc[curr.strategy_name]) {
+            acc[curr.strategy_name] += 1;
+        } else {
+            acc[curr.strategy_name] = 1;
+        }
+        return acc;
+    }, {})
+
+    const arrData = Object.entries(formattedData);
+
+    arrData.forEach(([strat, count]) => {
+        pieLabels.push(strat);
+        pieData.push(count);
+    });
+    console.log('pieLabels', pieLabels)
+    console.log('pieData', pieData)
+
+
+    return { pieData, pieLabels };
+}
+
+
