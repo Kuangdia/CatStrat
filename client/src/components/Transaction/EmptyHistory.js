44ea7d91ff6dc3626c111dbb9067cbebd65c8cde
Diana <slorrrr@gmail.com>
2022-02-21 20:04:20 -0500

    fix compare bug not showing

--- client/src/components/Transaction/EmptyHistory.js
+++ client/src/components/Transaction/EmptyHistory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 export default function EmptyHistory() {
@@ -7,14 +6,4 @@ export default function EmptyHistory() {
       <h1>NO transaction history yet!</h1>
     </div>
   );
-=======
-import React from "react";
-
-export default function EmptyHistory() {
-  return (
-    <div>
-      <h1>NO transaction history yet!</h1>
-    </div>
-  );
->>>>>>> 2ddc48e0951ac54cff19413424cd6ac75bb73b6e
 }
--- client/src/components/Transaction/HistoryList.js
+++ client/src/components/Transaction/HistoryList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import History from "./History";
 
@@ -40,46 +39,3 @@ export default function HistoryList(props) {
     </>
   );
 }
-=======
-import React from "react";
-import History from "./History";
-
-
-// target_strategy: null
-// target_user: null
-// user_id: 1
-
-export default function HistoryList(props) {
-  const { historyData } = props;
-
-  const historyComponents = historyData.map(historyObj => {
-    return (
-      <History
-        key={ historyObj.id }
-        id={ historyObj.id }
-        is_spending={ historyObj.is_spending }
-        desc={ historyObj.description }
-        amount={ historyObj.amount }
-        date={ historyObj.created_at.replace("T", " ").split(".")[0] }
-        username={ historyObj.username }
-        strategy_name={ historyObj.strategy_name }
-        unlock_chart={ historyObj.unlock_chart }
-        unlock_strategies={ historyObj.unlock_strategies }
-      />
-    )
-  })
-  return (
-    <>
-      <h2 className="history__title">CateCoins Transaction</h2>
-      <table className="history__table">
-        <thead className="history__table__header">
-          <History id="" is_spending="" desc="DESCRIPTION" amount="AMOUNT" date="DATE" />
-        </thead>
-        <tbody className="history__table__body">
-          { historyComponents }
-        </tbody>
-      </table>
-    </>
-  );
-}
->>>>>>> 2ddc48e0951ac54cff19413424cd6ac75bb73b6e
