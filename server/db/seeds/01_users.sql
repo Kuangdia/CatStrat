6cacde098b44d74601c3d2136cdfb1d29fa518e0
James <jameshuang337@gmail.com>
2022-02-22 23:08:19 -0800

    update seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,8 +1,11 @@
-INSERT INTO users (username, password, email, money_bracket, avatar_url)
+INSERT INTO users (username, password, email, money_bracket, avatar_url, coins)
 VALUES 
-('JohnTheTrader', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '111@gmail.com', '5', 'https://i.imgur.com/cZ4rd8C.png'),
-('alice_trades', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '222@gmail.com', '2', 'https://www.kindpng.com/picc/m/747-7475396_kitty-kitten-cat-white-kawaii-soft-mochi-chibi.png'),
-('Kevin', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '333@gmail.com', '3', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg'),
-('Joe', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '444@gmail.com', '5', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg'),
-('Billy', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '555@gmail.com', '6', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg');
+('JimmyTrades', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '111@gmail.com', '5', 'https://cdn.imgbin.com/13/6/23/imgbin-llama-desktop-pack-animal-discount-live-57V12tLGD0Z4HLuuF1MqhBk6W.jpg',743),
+('Llama_bets', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '222@gmail.com', '6', 'https://us.123rf.com/450wm/fizkes/fizkes2007/fizkes200701872/152319944-close-up-headshot-portrait-of-smiling-vietnamese-young-woman-look-at-camera-talk-on-video-call-profi.jpg?ver=6', 621),
+('Kevin', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '333@gmail.com', '3', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg', 581),
+('Joe', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '444@gmail.com', '5', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg', 200),
+('Billy', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '555@gmail.com', '6', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg', 129),
+('Luke', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '666@gmail.com', '6', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg', 54),
+('Helga', '$2b$10$vTX7afCDZkLU97kpGBn04OrifZi5EMvVrAijacvqj4QnNvbrZaBvW', '777@gmail.com', '8', 'https://ih1.redbubble.net/image.805767906.8098/st,small,507x507-pad,600x600,f8f8f8.u1.jpg', 90);
+
 
